c4552a4a38d09817f719ad3e70dac36c32ad38ae
Rob Uttley <rob@robsoft.net>
2021-10-17 20:29:10 +0100

    tweaks

--- first_fetch.sh
+++ first_fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # edit the line below!
-git clone https://rob@robsoft.net:"_fg5K25o0kpCXUvy3830DWEoi24J2bI19r721BS5N410447 global horcrux pattern"@github.com/robsoft/PiTest
+git clone https://user:ghp_fg5K25o0kp3830CXUvyDWEoi24J2bI19r721BS5N410447@github.com/robsoft/PiTest
+
 
--- setup_git.sh
+++ setup_git.sh
@@ -2,4 +2,5 @@
 git config --global credential.helper store
 git config --global user.email "rob@robsoft.net"
 git config --global user.name "Rob Uttley"
+sudo  chmod +777 first_fetch.sh
 echo "edit first_fetch.sh accordingly and then run it"
